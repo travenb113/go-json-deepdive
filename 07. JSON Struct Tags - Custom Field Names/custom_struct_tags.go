ceb2462f6132280f5d587d097f7c99f1abd59f61
travenb113 <Overwhelmingevil@proton.me>
2025-05-25 19:37:08 +0300

    Add example using custom JSON field names via struct tags

---
+++ 07. JSON Struct Tags - Custom Field Names/custom_struct_tags.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Car struct {
+	Species     string `json:"carType"`
+	Description string `json:"what it does"`
+}
+
+func main() {
+	carJson := `{"carType": "passenger car", "what it does": "drive on the road slowly/fast"}`
+	var car Car
+	json.Unmarshal([]byte(carJson), &car)
+	fmt.Println(car)
+}
